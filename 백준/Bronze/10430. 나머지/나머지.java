ac8c290e9beb46c2bd5022607dfd91d459a3cadf
yebon <133661980+ybon1107@users.noreply.github.com>
2025-04-22 14:43:33 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(token.nextToken());
+        long b = Long.parseLong(token.nextToken());
+        long c = Long.parseLong(token.nextToken());
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
