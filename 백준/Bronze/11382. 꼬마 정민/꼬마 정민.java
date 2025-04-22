ed5fcd90b99f219d7bf2ad2724184b8be3a5da44
yebon <133661980+ybon1107@users.noreply.github.com>
2025-04-22 14:58:14 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.StringTokenizer;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(token.nextToken());
+        long b = Long.parseLong(token.nextToken());
+        long c = Long.parseLong(token.nextToken());
+
+        System.out.println(a+b+c);
+    }
+}
