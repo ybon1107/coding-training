da8334dafa79bd9e002547db3edc85123606be6b
yebon <133661980+ybon1107@users.noreply.github.com>
2025-04-22 11:43:32 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String line = br.readLine();
+
+        String[] tokens = line.split(" ");
+
+        Long a = Long.parseLong(tokens[0]);
+        Long b = Long.parseLong(tokens[1]);
+
+        System.out.println(a+b);
+    }
+}
