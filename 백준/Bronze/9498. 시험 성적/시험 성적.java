f2f3443868263cd1829efda1002aa4d8f8ee542b
yebon <133661980+ybon1107@users.noreply.github.com>
2025-04-22 16:25:16 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine()); 
+
+        if(a>=90){
+            System.out.println("A");
+        } else if(a>=80){
+            System.out.println("B");
+        } else if(a>=70){
+            System.out.println("C");
+        } else if(a>=60){
+            System.out.println("D");
+        } else{
+            System.out.println("F");
+        }
+    }
+}
