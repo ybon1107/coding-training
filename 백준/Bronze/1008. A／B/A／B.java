097743224a17158e9762c8f6b87dfbf646ee6750
yebon <133661980+ybon1107@users.noreply.github.com>
2025-04-22 14:15:21 +0900

    [Bronze V] Title: A/B, Time: 112 ms, Memory: 14616 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+
+        System.out.printf("%.15f\n", a / b);
+    }
+}
