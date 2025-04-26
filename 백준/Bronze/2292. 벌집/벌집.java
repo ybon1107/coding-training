1d5638c703d0eb1f2dbd357269a0c41c429b7dbc
yebon <133661980+ybon1107@users.noreply.github.com>
2025-04-26 13:11:07 +0900

    [Bronze II] Title: 벌집, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -5,23 +5,21 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int index = 0;
-        int n = 0;
-        while(N >=0){
-            n = index*(index+1)/2;
-            int result =(N-2) - (6*n);
-            if(result >= 0){
-                index++;
-            } else{
-                N = -1;
-            }
+        int count = 1;
+        int range = 1;
+
+        while(N>range){
+            range += 6*count;
+            count++;
         }
-        System.out.println(index+1);
+        System.out.println(count);
+        
     }
 }
 
+// 1 1
 // 2 2~ 
 // 3 2+6(1)~
-// 4 2+6(1+2)~
-// 5 2+6(1+2+3)~
-// 6 2+6(1+2+3+4)~
+// 4 2+6(1)+6(2)~
+// 5 2+6(1)+6(2)+6(3)~
+// 6 2+6(1)+6(2)+6(3)+6(4)~
