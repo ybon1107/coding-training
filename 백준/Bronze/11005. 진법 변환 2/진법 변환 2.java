6bf2ceb835c36edc49b89797031fd6b281164d67
yebon <133661980+ybon1107@users.noreply.github.com>
2025-05-04 21:29:14 +0900

    [Bronze I] Title: 진법 변환 2, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(token.nextToken());
+        int B = Integer.parseInt(token.nextToken());
+        System.out.println(Integer.toString(N,B).toUpperCase());
+
+    }
+}
