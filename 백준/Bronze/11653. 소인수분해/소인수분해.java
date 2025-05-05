9d7d388327f22b7bbedf894b2fd3cff6caffdbe9
yebon <133661980+ybon1107@users.noreply.github.com>
2025-05-05 15:11:14 +0900

    [Bronze I] Title: 소인수분해, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -4,12 +4,14 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int max = N;
-        for(int i = 2; i <= max; i++){
+        for(int i = 2; i*i <= N; i++){
             while(N % i == 0) {
                 System.out.println(i);
                 N = N/i;
             }
+        }
+        if (N > 1) {
+            System.out.println(N);
         }
     }
 }
