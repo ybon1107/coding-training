642dc6fe425b99a6408eb93352c28fd8ac801078
yebon <133661980+ybon1107@users.noreply.github.com>
2025-05-12 09:58:33 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 64 ms, Memory: 11408 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
