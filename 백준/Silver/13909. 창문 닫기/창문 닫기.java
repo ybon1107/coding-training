bc98ae6ad3b198ac6d84527cb404a78f3b6cd863
yebon <133661980+ybon1107@users.noreply.github.com>
2025-05-23 17:57:51 +0900

    [Silver V] Title: 창문 닫기, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((int)Math.sqrt(N));
+
+    }
+}
